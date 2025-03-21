80114edd1aeb1abb96683e957e052aad2893435e
MHRoni-dev <mdmonirhossain.github@gmail.com>
2025-03-21 19:33:15 +0600

    Add todoService for API requests: create, fetch, update, and delete Todos

---
+++ app/feature/todoService.ts
@@ -0,0 +1,12 @@
+import { CreateTodo, Todo } from '@/app/api/todo/schema';
+import axios from 'axios';
+
+
+export const createTodoReq = async (todo: CreateTodo) => await axios.post('/api/todo', todo);
+
+export const fetchAllTodosReq = async () => (await axios.get('/api/todo')).data as Todo[];
+
+export const updateTodoReq = async (todo: Todo) => axios.put(`/api/todo/${todo._id}`, todo);
+
+export const deleteTodoReq = async (todoId: string) => axios.delete(`/api/todo/${todoId}`);
+
