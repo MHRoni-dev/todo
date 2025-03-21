a6ec04610ebbc6a918a6563574843472232dc171
MHRoni-dev <mdmonirhossain.github@gmail.com>
2025-03-21 19:34:04 +0600

    Add ToggleTheme component for dark mode functionality

---
+++ components/theme/ToggleTheme.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+import { Switch } from '../ui/switch'
+
+export default function ToggleTheme() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true'
+    if(isDarkMode) {
+      document.documentElement.classList.add('dark')
+      setDarkMode(true)
+    }
+
+  }, [])
+
+  const handleToggleDarkMode = () => {
+    localStorage.setItem('darkMode', String(!darkMode))
+    if(darkMode) {
+      document.documentElement.classList.remove('dark')
+      setDarkMode(false)
+    } else {
+      document.documentElement.classList.add('dark')
+      setDarkMode(true)
+    }
+  }
+    
+
+  return (
+    <div className='flex justify-end mb-4'>
+      <p className='flex items-center gap-2'>Dark Mode: <Switch checked={darkMode} onClick={handleToggleDarkMode}/>  </p>
+    </div>
+  )
+}
