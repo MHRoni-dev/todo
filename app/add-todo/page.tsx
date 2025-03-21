202e1854a21278210b1a6a31e113824ff6596165
MHRoni-dev <mdmonirhossain.github@gmail.com>
2025-03-21 19:35:04 +0600

    Add AddTodoForm to Home page and restructure layout for Todos display

---
+++ app/add-todo/page.tsx
@@ -0,0 +1,9 @@
+import AddTodoForm from './components/AddTodoForm';
+
+export default function page() {
+  return (
+    <div className='grid place-items-center min-h-screen'>
+      <AddTodoForm />
+    </div>
+  )
+}
