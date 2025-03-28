523329006c8d99d4f7be08eacaac648a9d43a6ae
MHRoni-dev <mdmonirhossain.github@gmail.com>
2025-03-28 11:32:35 +0600

    Add JWT utility for signing and verifying tokens

---
+++ lib/token.ts
@@ -0,0 +1,30 @@
+import { config } from '@/config';
+import JWT, { JwtPayload as JWTJwtPayload } from 'jsonwebtoken';
+
+
+export interface JwtPayload extends JWTJwtPayload {
+  _id : string;
+  username: string;
+}
+
+
+export default class Token {
+  static sign = (payload: JwtPayload): string => {
+    return JWT.sign(payload, config.default.JWT.JWT_SECRET, { expiresIn: config.default.JWT.JWT_EXPIRES_IN });
+  }
+
+  static verify = (token: string): JwtPayload => {
+   try {
+    const decoded = JWT.verify(token, config.default.JWT.JWT_SECRET);
+    if(typeof decoded === 'string') {
+      throw new Error('Invalid token');
+    }
+
+
+    return decoded as JwtPayload;
+
+   } catch {
+    throw new Error('Invalid  token');
+   }
+  }
+}
