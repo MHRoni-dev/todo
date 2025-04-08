e69d5c49e3cf09a24db3c6848fd00a10d1b46ae7
MHRoni-dev <mdmonirhossain.github@gmail.com>
2025-04-08 12:49:22 +0600

    Add next-transpile-modules dependency and configure webpack alias

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+import path from 'path';
+
+const nextConfig = {
+  /* config options here */
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...(config.resolve?.alias || {}),
+        '@': path.resolve(process.cwd()),
+      },
+    };
+    return config;
+  }
+};
+
+export default nextConfig;
